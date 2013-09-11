a3c308a91e507d1d326849af92beab3840bfba0e
Michael Banken <michael.banken@mathe.stud.uni-erlangen.de>
2013-09-11 10:49:42 +0200

    added include guards

--- constants.h
+++ constants.h
@@ -1,3 +1,6 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
 #define MSQ_ALIGNMENT 64
 #define INLINE_ATTR
 
@@ -6,4 +9,6 @@
 
 typedef long MSQ_DCAS_TYPE;
 typedef unsigned int uint_sys;
+
+#endif
 
