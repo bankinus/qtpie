0f4bbd9aba79adda6d49c4185d6021b0303379a2
Lorenz Haspel <lorenz@badgers.com>
2013-09-13 11:27:17 +0200

    cleaned up testreihe
    added macros to significantly improve readability

--- testreihe.cpp
+++ testreihe.cpp
@@ -6,181 +6,81 @@
 #include "queues.h"
 
 
-#define NUMBA 1<<25
 //#define WOSCH
-#define HASWELL
+//#define HASWELL
+
+
 
 using namespace std;
 
+#define testround(Type, elements, threads, ops) { \
+	long time;	\
+	Type *queue = new Type();	\
+	Test_round t(queue,elements,threads,ops);	\
+	cout << "test round mit "<< #Type <<" beginnt. \tThreads: " << threads << "\tOperationen: " << ops << "\tElemente zu Beginn: " << elements << endl;	\
+	time = t.starttest();	\
+	cout << "Zeit benoetigt: " << time << "\tms" << endl;	\
+	t.cleanup();	\
+	delete queue;	\
+}
+
+#define testmpsc(Type, threads, ops) {	\
+	long time;	\
+	Type *queue = new Type();	\
+	Test_mpmc t(queue,threads,ops);	\
+	cout << "test mpsc mit\t"<< #Type <<" beginnt.\tProducers: " << threads << "\tOperationen: " << ops << endl;	\
+	time = t.starttest();	\
+	cout << "Zeit benoetigt: " << time << "ms" << endl;	\
+	t.cleanup();	\
+	delete queue;	\
+}
+
+
 int main(){
 	
-	long time;
+
 
 //test_round erstellen
 
 //blocking queue
 
+	long elements;
+	int threads;
+	long ops;
+	
 
-	SpinQueue *spin2 = new SpinQueue();
-	Test_round t1(spin2,20,5,NUMBA);
-	cout << "test round mit spinqueue beginnt" << endl;
-	time = t1.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t1.cleanup();
-	delete spin2;
+	elements = 4;
+	threads= 2;
+	ops = 1<<25;
 
-	MutexQueue *mut2 = new MutexQueue();
-	Test_round t6(mut2,20,5,NUMBA);
-	cout << "test round mit mutexqueue beginnt" << endl;
-	time = t6.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t6.cleanup();
-	delete mut2;
-
-	TLMutexQueue *tlm2 = new TLMutexQueue();
-	Test_round t7(tlm2,20, 5, NUMBA);
-	cout << "test round mit tlmqueue beginnt" << endl;
-	time = t7.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t7.cleanup();
-	delete tlm2;
-
-	TLSpinQueue *tls2 = new TLSpinQueue();
-	Test_round t8(tls2,20,5, NUMBA);
-	cout << "test round mit tlsqueue beginnt" << endl;
-	time = t8.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t8.cleanup();
-	delete tls2;
-
-//nonblocking queues
-
-	MSQueue *msq2 = new MSQueue();
-	Test_round t2(msq2,20, 5, NUMBA);
-	cout << "test round mit msqueue beginnt" << endl;
-	time = t2.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t2.cleanup();
-	delete msq2;
-
+	testround(SpinQueue, elements, threads, ops);
+	testround(MutexQueue, elements, threads, ops);
+	testround(TLMutexQueue, elements, threads, ops);
+	testround(TLSpinQueue , elements, threads, ops);
+	testround(MSQueue , elements, threads, ops);
 #ifdef WOSCH
-	WnbsQueue *wq2 = new WnbsQueue();
-	Test_round t3(wq2,20, 1, NUMBA);
-	cout << "test round mit woschqueue beginnt" << endl;
-	time = t3.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t3.cleanup();
-	delete wq2;
+	testround(WnbsQueue , elements, threads, ops);
 #endif
-
-
 #ifdef HASWELL
-	//transaction queue
-
-	TransQueue *trans2 = new TransQueue();
-	Test_round t5(trans2,20, 5, NUMBA);
-	cout << "test round mit transqueue beginnt" << endl;
-	time = t5.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t5.cleanup();
-	delete trans2;
-
-	TLTransQueue *tlt2 = new TLTransQueue();
-	Test_round t9(tlt2, 20, 5, NUMBA);
-	cout << "test round mit tltqueue beginnt" << endl;
-	time = t9.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	t9.cleanup();
-	delete tlt2;
+	testround(TransQueue , elements, threads, ops);
+	testround(TLTransQueue , elements, threads, ops);
 #endif
-//test_mpmc erstellen
 
 
-//blocking queue
+	ops = 1<<20;
 
-
-	SpinQueue *spin1 = new SpinQueue();
-	Test_mpmc c1(spin1, 5, NUMBA);
-	cout << "test msmc mit spinqueue beginnt" << endl;
-	time = c1.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c1.cleanup();
-	delete spin1;
-
-	MutexQueue *mut1 = new MutexQueue();
-	Test_mpmc c6(mut1, 5, NUMBA);
-	cout << "test msmc mit mutexqueue beginnt" << endl;
-	time = c6.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c6.cleanup();
-	delete mut1;
-
-	TLMutexQueue *tlm1 = new TLMutexQueue();
-	Test_mpmc c7(tlm1, 5, NUMBA);
-	cout << "test msmc mit tlmqueue beginnt" << endl;
-	time = c7.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c7.cleanup();
-	delete tlm1;
-
-	TLSpinQueue *tls1 = new TLSpinQueue();
-	Test_mpmc c8(tls1, 5, NUMBA);
-	cout << "test msmc mit tlsqueue beginnt" << endl;
-	time = c8.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c8.cleanup();
-	delete tls1;
-
-
-//nonblocking queues
-
-
-	MSQueue *msq1 = new MSQueue();
-	Test_mpmc c2(msq1, 5, NUMBA);
-	cout << "test msmc mit msqueue beginnt" << endl;
-	time = c2.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c2.cleanup();
-	delete msq1;
- 
+	testmpsc(SpinQueue, threads, ops);
+	testmpsc(MutexQueue, threads, ops);
+	testmpsc(TLMutexQueue, threads, ops);
+	testmpsc(TLSpinQueue , threads, ops);
+	testmpsc(MSQueue , threads, ops);
+	testmpsc(MPSCQueue, threads, ops);
 #ifdef WOSCH
-	WnbsQueue *wq1 = new WnbsQueue();
-	Test_mpmc c3(wq1, 1, NUMBA);
-	cout << "test msmc mit woschqueue beginnt" << endl;
-	time = c3.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c3.cleanup();
-	delete wq1;
+	testmpsc(WnbsQueue,  threads, ops);
 #endif
-
-	MPSCQueue *mpq1 = new MPSCQueue();
-	Test_mpmc c4(mpq1, 5, NUMBA);
-	cout << "test msmc mit mpscqueue beginnt" << endl;
-	time = c4.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c4.cleanup();
-	delete mpq1;
-
 #ifdef HASWELL
-	//transaction queue
-
-	TransQueue *trans1 = new TransQueue();
-	Test_mpmc c5(trans1, 5, NUMBA);
-	cout << "test msmc mit transqueue beginnt" << endl;
-	time = c5.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c5.cleanup();
-	delete trans1;
-
-
-	TLTransQueue *tlt1 = new TLTransQueue();
-	Test_mpmc c9(tlt1, 5, NUMBA);
-	cout << "test msmc mit tltqueue beginnt" << endl;
-	time = c9.starttest();
-	cout << "Zeit benoetigt: " << time << "ms" << endl;
-	c9.cleanup();
-	delete tlt1;
+	testmpsc(TransQueue, threads, ops);
+	testmpsc(TLTransQueue , threads, ops);
 #endif
-
 
 }
